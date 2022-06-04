526620d2e2723375aeba289da6dc70236bfc2a45
Spantil <106875237+Spantil@users.noreply.github.com>
2022-06-04 17:10:31 +0300

    Create privacypolicy.php

---
+++ inf/privacypolicy.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="ru">
+    <head>
+    <meta charset="utf-8" />
+
+    <title>Прием обращений</title>
+    <link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/style.css"/>
+    <link rel="stylesheet" href="css/adaptive.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <style>
+    </style>
+    </head>
+
+    <body>
+
+        <div class="container  d-flex flex-wrp justify-content-center mt-3" >
+            <div class="row w-50 border rounded-top p-3 mb-5 ">
+                <div class=" col">
+                    <img class=" rounded mx-auto d-block mb-5 " src="..\img\gerb2.png" alt="" width="110" height="150">
+                </div>
+            
+                <h4 class="text-center">Условия использования Единой системы идентификации и аутентификации</h4>
+
+                <div class="mb-5 mt-3">
+                    <div class="accordion accordion-flush" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Общие положения
+                            </button>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                                 <ol>
+                                    <li>Настоящие Условия использования регулируют отношения пользователей с оператором Единой системы идентификации и аутентификации (далее — ЕСИА)</li>
+                                    <li>Оператором ЕСИА является Министерство связи и массовых коммуникаций Российской Федерации</li>
+                                    <li>Пользователем ЕСИА является любое физическое лицо или сотрудник юридического лица, зарегистрированные в ЕСИА</li>
+                                </ol>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Регистрация пользователей в ЕСИА
+                        </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                                    <ol>
+                                        <li>Любой пользователь сети Интернет может бесплатно зарегистрировать учётную запись пользователя ЕСИА</li>
+                                        <li>Для регистрации в ЕСИА пользователь должен указать следующие контактные данные (на выбор):</li>
+                                            <ul>
+                                                <li>действительный номер мобильного телефона</li>
+                                                <li>действительный адрес электронной почты</li>
+                                                <li>При регистрации пользователь должен подтвердить, что он имеет доступ к указанному номеру мобильного телефона или адресу электронной почты</li>
+                                            </ul>
+                                        <li>Зарегистрированный пользователь ЕСИА может подтвердить свою личность одним из следующих способов:</li>
+                                            <ul>
+                                                <li>посредством получения кода подтверждения личности</li>
+                                                <li>посредством подтверждения личности в уполномоченной организации</li>
+                                                <li>с помощью электронной подписи</li>
+                                                <li>с помощью карты УЭК</li>
+                                            </ul>
+                                        <li>Для получения кода подтверждения личности пользователь должен указать действительные полное имя (фамилию, имя, отчество), личные данные (пол и дату рождения) страховой номер индивидуального лицевого счёта (СНИЛС), а также данные документа, удостоверяющего личность. Код подтверждения личности может быть получен в регистрируемом почтовом отправлении через ФГУП «Почта России»</li>
+                                        <li>Для подтверждения личности зарегистрированный пользователь может обратиться:</li>
+                                            <ul>
+                                                <li>в центры продаж и обслуживания клиентов ОАО «Ростелеком»</li>
+                                                <li>посредством подтверждения личности в уполномоченной организации</li>
+                                                <li>с помощью электронной подписи</li>
+                                            </ul>
+                                            Для подтверждения личности в уполномоченной организации пользователь должен предъявить документ, удостоверяющий его личность
+                                        <li>Электронная подпись должна соответствовать требованиям Федерального закона от 6 апреля 2011 г. № 63-ФЗ «Об электронной подписи», предъявляемым к усиленной квалифицированной электронной подписи. Сертификат ключа проверки электронной подписи должен содержать полное имя и страховой номер индивидуального лицевого счёта (СНИЛС) пользователя</li>
+                                    </ol>  
+                        </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Права и обязанности пользователя
+                        </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">     
+                                <ol>
+                                    <li>Настоящие Условия использования регулируют отношения пользователей с оператором Единой системы идентификации и аутентификации (далее — ЕСИА)</li>
+                                    <li>Оператором ЕСИА является Министерство связи и массовых коммуникаций Российской Федерации</li>
+                                    <li>Пользователем ЕСИА является любое физическое лицо или сотрудник юридического лица, зарегистрированные в ЕСИА</li>
+                                    <li>Пользователь ЕСИА вправе:
+                                        <ul>
+                                            <li>использовать свою учётную запись в ЕСИА для санкционированного доступа к информации, содержащейся в государственных, муниципальных и других информационных системах</li>
+                                            <li>удалить свою учётную запись</li>
+                                        </ul>
+                                    </li>
+                                    <li>Пользователь ЕСИА обязан:</li>  
+                                        <ul>
+                                            <li>не производить действия, направленные на нарушение процесса функционирования ЕСИА или информационных систем, подключённых к ЕСИА</li>
+                                            <li>соблюдать требования «Политики конфиденциальности ЕСИА»</li>
+                                        </ul>
+                                </ol>   
+                        </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingFour">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                            Права и обязанности оператора
+                        </button>
+                        </h2>
+                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <ol>
+                                <li>Оператор ЕСИА вправе:</li>
+                                    <ul>
+                                        <li>требовать от пользователя подтверждения основания получения доступа к информационным системам, подключённым к ЕСИА (если пользователь использует для доступа учётную запись ЕСИА)</li>
+                                        <li>проводить мониторинг действий пользователя в ЕСИА</li>
+                                        <li>ограничивать доступ пользователя к информационным системам, подключённым к ЕСИА (если пользователь использует для доступа учётную запись ЕСИА), в том числе путём блокировки учётной записи пользователя в ЕСИА</li>
+                                        <li>изменить настоящие условия использования ЕСИА без предварительного уведомления пользователя</li>
+                                        <li>направлять пользователю информационные сообщения</li>
+                                    </ul>
+                                <li>Оператор ЕСИА обязан:</li>
+                                    <ul>
+                                        <li>обеспечить функционирование ЕСИА, в том числе бесперебойную работу функции идентификации и аутентификации пользователей ЕСИА при доступе к информационным системам, подключённым к ЕСИА</li>
+                                        <li>гарантировать целостность и неизменность данных пользователей в ЕСИА</li>
+                                        <li>вести учёт и статистику обращений пользователей к ЕСИА</li>
+                                        <li>обеспечить защиту персональных данных пользователя в соответствии с требованиями законодательства РФ и «Политикой конфиденциальности ЕСИА»</li>
+                                    </ul>
+                            </ol>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                    <div class="container text-center  mt-1">
+                            <p><a class="btn btn-lg btn-outline-primary" href="../index.php">Назад</a></p>
+                    </div>
+            </div>
+        </div>
+
+
+</html>
